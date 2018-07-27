35ab915caf8ef4525f06f1785791afdbfe990037
paubric <paubric@gmail.com>
2018-07-27 08:10:09 +0000

    latest

--- test.py
+++ test.py
@@ -10,13 +10,13 @@ address = './61944293.png'
 
 img, filename = coconet.load_image(address)
 
-X = coconet.generate_placeholder_tensor(picture_sizex, picture_sizey)
-X_SR = coconet.generate_placeholder_tensor(picture_sizex, picture_sizey, enhance = enhance)
-Y = coconet.generate_value_tensor(img, picture_sizex, picture_sizey)
-model = coconet.generate_model_dense([100] * 10)
+X = coconet.generate_placeholder_tensor(picture_sizex, picture_sizey, trimensional = True)
+X_SR = coconet.generate_placeholder_tensor(picture_sizex, picture_sizey, trimensional = True, enhance = enhance)
+Y = coconet.generate_value_tensor(img, picture_sizex, picture_sizey, trimensional = True)
+model = coconet.generate_model_conv([32] * 10, dim = 7, slen = 1)
 
 
-history = model.fit(X, Y, epochs = 10000, batch_size = 49152, shuffle = True)
+history = model.fit(X, Y, epochs = 1000)
 #history = model.fit(X, Y, epochs = 1000, batch_size = 1024)
 prediction = coconet.predict(model, X_SR, picture_sizex * enhance, picture_sizey * enhance)
 
