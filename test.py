c549c6044f1dc3027fd5a132d71bf00ada2ca6fa
paubric <paubric@gmail.com>
2018-07-27 08:13:45 +0000

    legacy

--- test.py
+++ test.py
@@ -1,23 +1,31 @@
 import coconet
+from tkinter.filedialog import askopenfilename
+import matplotlib.pyplot as plt
 import cv2
 
-picture_sizex = 192
-picture_sizey = 256
-#enhance = 1
-enhance = 4
+picture_sizex = 32
+picture_sizey = 32
+enhance = 1
+#enhance = 4
 
-address = './61944293.png'
+address = askopenfilename()
 
 img, filename = coconet.load_image(address)
 
-X = coconet.generate_placeholder_tensor(picture_sizex, picture_sizey, trimensional = True)
-X_SR = coconet.generate_placeholder_tensor(picture_sizex, picture_sizey, trimensional = True, enhance = enhance)
-Y = coconet.generate_value_tensor(img, picture_sizex, picture_sizey, trimensional = True)
-model = coconet.generate_model_conv([32] * 10, dim = 7, slen = 1)
+X = coconet.generate_placeholder_tensor(picture_sizex, picture_sizey)
+X_SR = coconet.generate_placeholder_tensor(picture_sizex, picture_sizey, enhance = enhance)
+Y = coconet.generate_value_tensor(img, picture_sizex, picture_sizey)
+model = coconet.generate_model_dense([100] * 10)
 
 
-history = model.fit(X, Y, epochs = 1000)
-#history = model.fit(X, Y, epochs = 1000, batch_size = 1024)
+#history = model.fit(X, Y, epochs = 1000, batch_size = 128, shuffle = True)
+history = model.fit(X, Y, epochs = 1000, batch_size = 1024)
 prediction = coconet.predict(model, X_SR, picture_sizex * enhance, picture_sizey * enhance)
+
+plt.subplot(121)
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.subplot(122)
+plt.imshow(cv2.cvtColor(prediction, cv2.COLOR_BGR2RGB))
+plt.show()
 
 coconet.save_image(prediction, address[:-4] + ' FUSED.png')
