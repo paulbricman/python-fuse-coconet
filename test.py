52cc5e17551262b822bb6fc448f62ae72f07bded
paubric <paubric@gmail.com>
2018-07-27 07:55:09 +0000

    latest

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
 import coconet
-from tkinter.filedialog import askopenfilename
-import matplotlib.pyplot as plt
 import cv2
 
-picture_sizex = 32
-picture_sizey = 32
-enhance = 1
-#enhance = 4
+picture_sizex = 192
+picture_sizey = 256
+#enhance = 1
+enhance = 4
 
-address = askopenfilename()
+address = './61944293.png'
 
 img, filename = coconet.load_image(address)
 
@@ -18,14 +16,8 @@ Y = coconet.generate_value_tensor(img, picture_sizex, picture_sizey)
 model = coconet.generate_model_dense([100] * 10)
 
 
-#history = model.fit(X, Y, epochs = 1000, batch_size = 128, shuffle = True)
-history = model.fit(X, Y, epochs = 1000, batch_size = 1024)
+history = model.fit(X, Y, epochs = 10000, batch_size = 49152, shuffle = True)
+#history = model.fit(X, Y, epochs = 1000, batch_size = 1024)
 prediction = coconet.predict(model, X_SR, picture_sizex * enhance, picture_sizey * enhance)
-
-plt.subplot(121)
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.subplot(122)
-plt.imshow(cv2.cvtColor(prediction, cv2.COLOR_BGR2RGB))
-plt.show()
 
 coconet.save_image(prediction, address[:-4] + ' FUSED.png')
